09d6c127a33d56201a8e5178430616b356017964
Mike Perez <thingee@gmail.com>
2013-07-31 15:26:46 -0700

    Remove xen storage manager tables
    This removes the unused tables sm_backend_config, sm_flavors and
    sm_volume. In addition, they're removed from the database API.
    
    Change-Id: I02ff6434c2d6928f2e2b08053f6dcb6178edd773
    Fixes: bug #1206962

--- cinder/db/sqlalchemy/models.py
+++ cinder/db/sqlalchemy/models.py
@@ -350,32 +350,6 @@ class Migration(BASE, CinderBase):
     status = Column(String(255))
 
 
-class SMFlavors(BASE, CinderBase):
-    """Represents a flavor for SM volumes."""
-    __tablename__ = 'sm_flavors'
-    id = Column(Integer(), primary_key=True)
-    label = Column(String(255))
-    description = Column(String(255))
-
-
-class SMBackendConf(BASE, CinderBase):
-    """Represents the connection to the backend for SM."""
-    __tablename__ = 'sm_backend_config'
-    id = Column(Integer(), primary_key=True)
-    flavor_id = Column(Integer, ForeignKey('sm_flavors.id'), nullable=False)
-    sr_uuid = Column(String(255))
-    sr_type = Column(String(255))
-    config_params = Column(String(2047))
-
-
-class SMVolume(BASE, CinderBase):
-    __tablename__ = 'sm_volume'
-    id = Column(String(36), ForeignKey(Volume.id), primary_key=True)
-    backend_id = Column(Integer, ForeignKey('sm_backend_config.id'),
-                        nullable=False)
-    vdi_uuid = Column(String(255))
-
-
 class Backup(BASE, CinderBase):
     """Represents a backup of a volume to Swift."""
     __tablename__ = 'backups'
@@ -429,9 +403,6 @@ def register_models():
     models = (Backup,
               Migration,
               Service,
-              SMBackendConf,
-              SMFlavors,
-              SMVolume,
               Volume,
               VolumeMetadata,
               SnapshotMetadata,
--- cinder/tests/test_migrations.py
+++ cinder/tests/test_migrations.py
@@ -800,3 +800,29 @@ class TestMigrations(test.TestCase):
                                        metadata,
                                        autoload=True)
             self.assertTrue('_name_id' not in volumes.c)
+
+    def test_migration_015(self):
+        """Test that dropping xen storage manager tables works correctly."""
+        for (key, engine) in self.engines.items():
+            migration_api.version_control(engine,
+                                          TestMigrations.REPOSITORY,
+                                          migration.INIT_VERSION)
+            migration_api.upgrade(engine, TestMigrations.REPOSITORY, 14)
+            metadata = sqlalchemy.schema.MetaData()
+            metadata.bind = engine
+
+            migration_api.upgrade(engine, TestMigrations.REPOSITORY, 15)
+            self.assertFalse(engine.dialect.has_table(engine.connect(),
+                                                      'sm_flavors'))
+            self.assertFalse(engine.dialect.has_table(engine.connect(),
+                                                      'sm_backend_config'))
+            self.assertFalse(engine.dialect.has_table(engine.connect(),
+                                                      'sm_volume'))
+
+            migration_api.downgrade(engine, TestMigrations.REPOSITORY, 14)
+            self.assertTrue(engine.dialect.has_table(engine.connect(),
+                                                     'sm_flavors'))
+            self.assertTrue(engine.dialect.has_table(engine.connect(),
+                                                     'sm_backend_config'))
+            self.assertTrue(engine.dialect.has_table(engine.connect(),
+                                                     'sm_volume'))
